c70b1b67b6019177a70ff88380177f8253ab7dee
TheSpax <72096329+TheSpax@users.noreply.github.com>
2021-11-29 03:11:33 +0100

    IT255 - DZ07

--- IT255-DZ05/src/app/components/form/form.component.ts
+++ IT255-DZ05/src/app/components/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Smestaj } from '../smestaj/smestaj.model';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,13 +10,35 @@ import { Smestaj } from '../smestaj/smestaj.model';
 export class FormComponent implements OnInit {
 
   @Output() formDataEmitter: EventEmitter<Smestaj>;
+  smestajForm: FormGroup
+  name: AbstractControl
+  desc: AbstractControl
+  roomNumber: AbstractControl
+  price: AbstractControl
 
-  constructor() {
+  constructor(formBuilder: FormBuilder) {
     this.formDataEmitter = new EventEmitter();
-  }
+    this.smestajForm = formBuilder.group({
+      'name': [null, Validators.compose([Validators.required, Validators.pattern(/^([\u00c0-\u01ffa-zA-Z&'-])+$/)])],
+      'desc': [null, Validators.required],
+      'roomNumber': [null, Validators.compose([Validators.required, Validators.pattern(/^\d+$/)])],
+      'price': [null, Validators.compose([Validators.required, Validators.pattern(/^\d+$/)])]
+    })
 
-  add(hotelName: HTMLInputElement, hotelDesc: HTMLTextAreaElement, roomNumber: HTMLInputElement, price: HTMLInputElement): void{
-    let smestaj = new Smestaj(hotelName.value, hotelDesc.value, parseFloat(roomNumber.value), parseFloat(price.value));
+    this.name = this.smestajForm.controls['name'];
+    this.desc = this.smestajForm.controls['desc'];
+    this.roomNumber = this.smestajForm.controls['roomNumber'];
+    this.price = this.smestajForm.controls['price'];
+    
+    this.desc.valueChanges.subscribe( (hotelDesc: string) =>  {
+      if(hotelDesc.length < 6){
+        console.log("Opis sadrzi " + hotelDesc.length + " karaktera. Minimum je 6.");
+      }
+    })
+  }
+  
+  add(smestajForm: any): void{
+    let smestaj = new Smestaj(smestajForm.name, smestajForm.desc, smestajForm.roomNumber, smestajForm.price);
     this.formDataEmitter.emit(smestaj);
   }
 
